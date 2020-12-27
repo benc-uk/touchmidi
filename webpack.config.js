e3e6c80f111484f4349331302776f6fd259e71b0
Ben Coleman <benc.uk@gmail.com>
2020-12-27 14:15:39 +0000

    webpack works

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,12 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require("path");
+
 module.exports = {
+  mode: "production",
   entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(process.cwd(), "dist"),
+  },
+  plugins: [new CleanWebpackPlugin()],
 };
